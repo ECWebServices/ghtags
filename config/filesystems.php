868c1a8e5c8113156d7d5451614345ae791f03e7
Elijah Cruz <ecwebservices@gmail.com>
2022-07-14 16:57:53 -0700

    Updated to fix issue where file isn't changed if not in the current directory.

--- config/filesystems.php
+++ config/filesystems.php
@@ -1,11 +1,14 @@
 <?php
 
+// If linux or mac, root it /, otherwise it's windows and we need to root it C:\
+$root = (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') ? 'C:' : '/';
+
 return [
     'default' => 'local',
     'disks' => [
         'local' => [
             'driver' => 'local',
-            'root' => getcwd(),
+            'root' => $root,
         ],
     ],
     'ghtags' => [
